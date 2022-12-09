7e0ec72df0b76bcd49e62f0789554f132df55edc
mihsamusev <mhs@bluepp.dk>
2022-12-09 10:48:07 +0100

    after black

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+
+Developer setup
+
+Before commit automatically run:
+    - flake8 linter
+    - pytest unittests
+
+Install pre-commit and the pre-commit hook from `.pre-commit-config.yaml`
+
+```
+pip install pre-commit
+pre-commit install
+```
+
--- src/module1.py
+++ src/module1.py
@@ -1,18 +1,29 @@
 from typing import Dict
 from dataclasses import dataclass
 from dataclasses import field
+
+
 @dataclass
 class Example:
     """
     Container for a example with name, unique id and content dictionary
     """
+
     name: str
     uuid: int
     content: Dict[str, int] = field(default_factory=dict)
+
     def get_content(self, key: str):
         return self.content.get(key)
+
+
 if __name__ == "__main__":
-    data = {"first": 13123, "second": 3333,"third": 33333,"fourth": 31351,"fifth": 31351,
+    data = {
+        "first": 13123,
+        "second": 3333,
+        "third": 33333,
+        "fourth": 31351,
+        "fifth": 31351,
     }
     example = Example("this", 2343, data)
     print(example)
